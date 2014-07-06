d109b30409820c6baa304c37f97d15aecb7ab327
Jon Carlson <jon@code42.com>
2014-07-05 19:33:59 -0500

    Added fileutil.go

---
+++ fileutil.go
@@ -0,0 +1,100 @@
+package fileutil
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "encoding/hex"
+    "path/filepath"
+    "crypto/rand"
+)
+
+/*
+ * Reads a file line by line into a channel
+ *
+ * c := readLinesChannel(fileName)
+ * for line := range c {
+ *   fmt.Printf("  Line: %s\n", line)
+ * }
+ */
+func ReadLinesChannel(fileName string) <-chan string {
+    c := make(chan string)
+    file, err := os.Open(fileName)
+    if err !=nil {
+        log.Panic(err)
+    }
+    go func() {
+        defer file.Close()
+        scanner := bufio.NewScanner(file)
+        for scanner.Scan(){
+            c <- scanner.Text()
+        }
+        close(c)
+    }()
+    return c
+}
+
+
+/*
+ * Reads a file line by line into an array
+ *
+ *  lines, err := readLinesArray(fileName)
+ *  if err != nil {
+ *      log.Fatalf("readLines: %s\n", err)
+ *  }
+ *  for i, line := range lines {
+ *      fmt.Printf("  Line: %d %s\n", i, line)
+ *  }
+ */
+func ReadLinesArray(path string) ([]string, error) {
+    file, err := os.Open(path)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+
+    var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    return lines, scanner.Err()
+}
+
+/*
+ * Writes the lines to the given file.
+ */
+func WriteLinesArray(lines []string, path string) error {
+    file, err := os.Create(path)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    w := bufio.NewWriter(file)
+    for _, line := range lines {
+        fmt.Fprintln(w, line)
+    }
+    return w.Flush()
+}
+
+// Returns whether or not the given file or directory exists
+func Exists(path string) (bool, error) {
+    _, err := os.Stat(path)
+    if err == nil {
+        return true, nil
+    }
+    if os.IsNotExist(err) {
+        return false, nil
+    }
+    return false, err
+}
+
+// Generates a temporary filename for use in testing or whatever
+func TempFileName(prefix, suffix string) string {
+    randBytes := make([]byte, 16)
+    rand.Read(randBytes)
+    return filepath.Join(os.TempDir(), prefix + hex.EncodeToString(randBytes) + suffix)
+}
