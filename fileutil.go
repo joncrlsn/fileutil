6b9deaf73cda8b8df1f0cec910b9d77a642f39d5
Jon Carlson <jon@code42.com>
2014-12-07 15:25:32 -0600

    cleanup with golint

--- fileutil.go
+++ fileutil.go
@@ -1,100 +1,95 @@
 package fileutil
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
-    "encoding/hex"
-    "path/filepath"
-    "crypto/rand"
+	"bufio"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 )
 
-/*
- * Reads a text file line by line into a channel.
- *
- * c := fileutil.ReadLinesChannel(fileName)
- * for line := range c {
- *   fmt.Printf("  Line: %s\n", line)
- * }
- */
+// ReadLinesChannel reads a text file line by line into a channel.
+//
+// c := fileutil.ReadLinesChannel(fileName)
+// for line := range c {
+//   fmt.Printf("  Line: %s\n", line)
+// }
+//
 func ReadLinesChannel(filePath string) <-chan string {
-    c := make(chan string)
-    file, err := os.Open(filePath)
-    if err != nil {
-        log.Panic(err)
-    }
-    go func() {
-        defer file.Close()
-        scanner := bufio.NewScanner(file)
-        for scanner.Scan(){
-            c <- scanner.Text()
-        }
-        close(c)
-    }()
-    return c
+	c := make(chan string)
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Panic(err)
+	}
+	go func() {
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			c <- scanner.Text()
+		}
+		close(c)
+	}()
+	return c
 }
 
-
-/*
- * Reads a text file line by line into an array.  Not recommended
- * for use with large files.
- *
- *  lines, err := fileutil.ReadLinesArray(filePath)
- *  if err != nil {
- *      log.Fatalf("readLines: %s\n", err)
- *  }
- *  for i, line := range lines {
- *      fmt.Printf("  Line: %d %s\n", i, line)
- *  }
- */
+// ReadLinesArray reads a text file line by line into an array.  Not recommended
+// for use with large files.
+//
+//  lines, err := fileutil.ReadLinesArray(filePath)
+//  if err != nil {
+//      log.Fatalf("readLines: %s\n", err)
+//  }
+//  for i, line := range lines {
+//      fmt.Printf("  Line: %d %s\n", i, line)
+//  }
+//
 func ReadLinesArray(path string) ([]string, error) {
-    file, err := os.Open(path)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
-    var lines []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        lines = append(lines, scanner.Text())
-    }
-    return lines, scanner.Err()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
 
-/*
- * Writes the lines to the given file.
- */
+// WriteLinesArray writes the lines to the given file.
 func WriteLinesArray(lines []string, path string) error {
-    file, err := os.Create(path)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-    w := bufio.NewWriter(file)
-    for _, line := range lines {
-        fmt.Fprintln(w, line)
-    }
-    return w.Flush()
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
 }
 
-// Returns whether or not the given file or directory exists
+// Exists returns whether or not the given file or directory exists
 func Exists(path string) (bool, error) {
-    _, err := os.Stat(path)
-    if err == nil {
-        return true, nil
-    }
-    if os.IsNotExist(err) {
-        return false, nil
-    }
-    return false, err
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
 
-// Generates a temporary filename for use in testing or whatever
+// TempFileName generates a temporary filename for use in testing or whatever
 func TempFileName(prefix, suffix string) string {
-    randBytes := make([]byte, 16)
-    rand.Read(randBytes)
-    return filepath.Join(os.TempDir(), prefix + hex.EncodeToString(randBytes) + suffix)
+	randBytes := make([]byte, 16)
+	rand.Read(randBytes)
+	return filepath.Join(os.TempDir(), prefix+hex.EncodeToString(randBytes)+suffix)
 }
